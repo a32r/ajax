f25b9b7cbabfc3f66e2a661f46870b97db6bcd68
Admin <site@mail.com>
2019-05-05 22:08:51 +0300

    Finished db_save.php file

--- php/db_save.php
+++ php/db_save.php
@@ -1,7 +1,12 @@
 <?php
 
-if(!empty($_POST))
-	debug($_POST);
-
+if(!empty($_POST)) {
+	$name = htmlspecialchars($_POST['name']);
+	$email = $_POST['email'];
+	$comment = htmlspecialchars($_POST['comment']);
+	$query = "INSERT INTO comments VALUES (NULL, ?, ?, ?)";
+	$stmt = $db->prepare($query);
+	$stmt->execute([$name, $email, $comment]);
+}
 
 ?>
